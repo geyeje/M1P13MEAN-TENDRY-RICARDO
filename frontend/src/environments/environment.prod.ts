23a7d7cc16fede67d5a2cfb9bae4641c8cab2731
geyeje <rahatendry@gmail.com>
2026-03-04 07:16:55 +0300

    fix api

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,6 +1,5 @@
 export const environment = {
   production: true,
   apiUrl: 'https://m1p13mean-tendry-ricardo.onrender.com/api',
-  stripePublicKey:
-    'pk_test_51T6GHF9bqT14xCYZ0VMGADcVAnHEx0gUiyEv8pdeW3icSX5XggPdmTktkDTC0zA3MkuzjtQ5f9SPWQnMNtjTBJnz001W4Yh6PA',
+  stripePublicKey: ''
 };
